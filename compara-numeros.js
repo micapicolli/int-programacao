9090d22f90c52b5282bcf7e65ad5eef743394fbb
micapicolli <carmo.micaela@escola.pr.gov.br>
2025-08-21 09:45:32 -0300

    compara-numeros.js

---
+++ compara-numeros.js
@@ -0,0 +1,10 @@
+// Solicita ao usuário que insira dois números
+let numero1 = parseFloat(prompt("Digite o 1º número: "));
+let numero2 = parseFloat(prompt("Digite o 2º número: "));
+
+// Verifica comparando se números são iguais
+if (numero1 === numero2) {
+    console.log("Os números são iguais.");
+} else {
+    console.log("Os números são diferentes.");
+}
